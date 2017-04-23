ae15c9ece08146742c2a964f7f7e90353edd3b60
AndreFCruz <andrecruz97@gmail.com>
2017-04-23 20:41:02 +0100

    First commit

---
+++ android/src/com/lpoo/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lpoo.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lpoo.game.Spheral;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Spheral(), config);
+	}
+}
