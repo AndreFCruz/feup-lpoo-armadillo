bbd035df4df307536510b621aae222b205a04102
AndreFCruz <andrecruz97@gmail.com>
2017-05-05 11:30:02 +0100

    MainMenu draft

--- core/src/com/lpoo/game/view/screens/MainMenuScreen.java
+++ core/src/com/lpoo/game/view/screens/MainMenuScreen.java
@@ -2,10 +2,20 @@ package com.lpoo.game.view.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.lpoo.game.Spheral;
 
 /**
@@ -15,37 +25,89 @@ import com.lpoo.game.Spheral;
 public class MainMenuScreen extends ScreenAdapter {
     private final Spheral game;
 
-    private OrthographicCamera camera;
-
     private Stage stage;
+    private Viewport viewport;
+    private Camera camera;
+    private SpriteBatch batch;
+    private Skin skin;
+    private TextureAtlas atlas;
 
     public MainMenuScreen(final Spheral game) {
         this.game = game;
+        this.batch = game.getBatch();
+
+        atlas = new TextureAtlas("comic-ui.atlas");
+        skin = new Skin(Gdx.files.internal("comic-ui.json"), atlas);
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false);
+        viewport = new ScreenViewport();
+        viewport.apply();
+        stage = new Stage(viewport, batch);
+
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+
+
+        Gdx.input.setInputProcessor(stage);
     }
+
+    @Override
+    public void show() {
+
+        Table table = new Table();
+        table.setFillParent(true);
+
+        table.center();
+
+        Label title = new Label("Spheral", skin);
+
+        //Create buttons
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton optionsButton = new TextButton("Options", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        //Add listeners to buttons
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameScreen());
+            }
+        });
+        exitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        //Add buttons to table
+        table.add(playButton);
+        table.row();
+        table.add(optionsButton);
+        table.row();
+        table.add(exitButton);
+
+
+        // Add table to stage
+        stage.addActor(table);
+    }
+
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
         camera.update();
-
-        SpriteBatch batch = game.getBatch();
-        batch.setProjectionMatrix(camera.combined);
-
-        batch.begin();
-        game.font.draw(batch, "Welcome to Spheral !", 100, 150);
-
-        if (Gdx.input.isTouched())
-            game.font.draw(batch, "YAYY", 100, 100);
-        else
-            game.font.draw(batch, "Tap anywhere to begin.", 100, 100);
-
-        batch.end();
-
     }
 
 }
